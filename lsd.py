7d8fbdcc83c7b8429b0780469768258eb2c4f9b6
Joseph Parkton <creekhippybear@gmail.com>
2017-07-02 15:50:49 -0700

    Updated
    Added better __doc__

--- lsd.py
+++ lsd.py
@@ -1,16 +1,27 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""This is a simple pythonic replacement for the windows 
-   'dir' command. Once added to the path and made an executable 
-   this can be used in any directory 'lsd' and look!"""
+"""This is a simple pythonic replacement for the windows 'dir' command. 
+
+
+   Once added to the Windows PATH environmental variable
+   And added to the PATHEXT environmental variable this
+   tool can be used in any directory 'lsd' and look! Otherwise
+   run in the same directory as this script as 'python lsd.py'
+   This assumes Python3 and the time is not correct yet."""
 
 import os 
 import win32api
 import time
-
 from colorama import init
 from termcolor import colored
+
+__author__ = "hippybear"
+__license__ = "MIT"
+__email__ = "yosi@codeblind.org"
+__copyright__ = "Copyright 2017, Code Blind"
+__status__ = "Development"
+
 
 init()
 
